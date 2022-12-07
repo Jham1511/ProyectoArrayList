07003378a6de7c5323914ac51cc22be00db573a2
Josue Ham <josue.ham@unitec.edu>
2022-12-06 22:37:42 -0600

    Menu con 2 opciones

---
+++ src/proyecto_arraylists/Proyecto_ArrayLists.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_arraylists;
+
+/**
+ *
+ * @author SKX Kafei
+ */
+public class Proyecto_ArrayLists {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu ventana = new Menu ();
+        ventana.setVisible(true);
+    }
+    
+}
